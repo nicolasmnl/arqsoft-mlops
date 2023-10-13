88bf3801838389802179562693d99e84a2693d6e
Andrielly Lucena <51917545+andriellyll@users.noreply.github.com>
2023-10-13 15:27:29 -0300

    Create retrain_production_model.py

---
+++ random-forest/retrain_production_model.py
@@ -0,0 +1,66 @@
+import mlflow
+from mlflow import MlflowClient
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_regression
+from sklearn.metrics import mean_squared_error
+
+import time
+
+mlflow.set_tracking_uri("http://localhost:5001")
+
+client = MlflowClient()
+
+X, y = make_regression(n_samples=400, n_features=4, n_informative=2, random_state=0, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model_uri = 'models:/sk-learn-random-forest-reg-model/Production'
+model = mlflow.sklearn.load_model(model_uri)
+
+prod_run_id = client.get_latest_versions("sk-learn-random-forest-reg-model", stages=["Production"])[0].run_id
+
+params = client.get_run(prod_run_id).data.params
+
+y_pred = model.predict(X_test)
+
+prod_mse = mean_squared_error(y_test, y_pred)
+
+with mlflow.start_run() as run:
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    new_mse = mean_squared_error(y_test, y_pred)
+
+    mlflow.log_params(params)
+    mlflow.log_metrics({"mse": mean_squared_error(y_test, y_pred)})
+
+new_run_id = run.info.run_id
+
+print(new_mse, prod_mse)
+
+if (new_mse < prod_mse):
+    name = "sk-learn-random-forest-reg-model"
+    # register new model version
+    model_uri = f"runs:/{new_run_id}/sklearn-model"
+    mv = client.create_model_version(name, model_uri, new_run_id)
+
+    to_prod_version = client.search_model_versions("run_id='{}'".format(new_run_id))[0].version
+    to_archive_version = client.search_model_versions("run_id='{}'".format(prod_run_id))[0].version
+
+    # Transition new model to Production stage
+    client.transition_model_version_stage(name, to_prod_version, "Production")
+
+    # Wait for the transition to complete
+    new_prod_version = client.get_model_version(name, to_prod_version)
+    while new_prod_version.current_stage != "Production":
+        new_prod_version = client.get_model_version(name, to_prod_version)
+        print('Transitioning new model... Current model version is: ', new_prod_version.current_stage)
+        time.sleep(1)
+
+    # Transition old model to Archived stage
+    client.transition_model_version_stage(name, to_archive_version, "Archived")
+
+else:
+    print('no improvement')
+
